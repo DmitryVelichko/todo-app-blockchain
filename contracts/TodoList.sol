6a94eabc35f51351744be9ec21f66c42c27ce3e1
Dmitry Velichko <soleil-2005@mail.ru>
2021-09-27 23:03:18 +0300

    New commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,10 +2,14 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
-}
+
 
 struct Task {
     uint id;
     string content;
     bool completed;
+}
+
+mapping(uint => Task) tasks;
+
 }
