dfb4b8ffaac690f50b115eaf54bb9c0eb2648dc7
Dmitry Velichko <soleil-2005@mail.ru>
2021-09-28 22:57:59 +0300

    New commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 App = {
     load: async() => {
         await App.loadWeb3();
+    },
+    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+  loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        await ethereum.enable()
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */})
+      } catch (error) {
+        // User denied account access...
+      }
     }
 }
 
